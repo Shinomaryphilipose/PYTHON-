7d666ff78cb12ceeacece47483e3148796abbab1
Shinomaryphilipose <shinosunil@gmail.com>
2024-10-26 09:49:36 +0530

    Add files via upload

---
+++ DataStructuresInPythonAssignment_3.py
@@ -0,0 +1,59 @@
+# Data structures in python
+#
+# Topic :List
+# Exercise
+# Q1. Create a list of 5 random numbers and print the list.
+
+#List =[34,25,10,45,12]
+#print(List)
+
+# Q2. Insert 3 new values to the list and print the updated list.
+# List.extend([52,'Kerala','India'])
+# print(List)
+
+# Q3. Try to use a for loop to print each element in the list.
+# for i in List:
+#     print(i)
+
+# Topic: Dictionary
+# Exercise
+# Q1. Create a dictionary with keys 'name', 'age', and 'address' and values
+# 'John', 25, and 'New York' respectively.
+
+#dic ={'name':'John','age':25,'address':'New York'}
+#print(dic)
+
+# Q2. Add a new key-value pair to the dictionary created in Q1 with key 'phone'
+# and value '1234567890'.
+
+# dic['phone']='1234567890'
+# print(dic)
+
+# Topic: Set
+# Exercise
+
+# Q1.Create a set with values 1, 2, 3, 4, and 5.
+#set1={1,2,3,4,5}
+#print(set1)
+
+# Q2. Add the value 6 to the set created in Q1.
+
+# set1={1,2,3,4,5}
+# set1.add(6)
+# print(set1)
+
+# Q3. Remove the value 3 from the set created in Q1.
+# set1={1,2,3,4,5}
+# set1.remove(3)
+# print(set1)
+# #
+# Topic:Tuple
+# Exercise
+
+# Q1. Create a tuple with values 1, 2, 3, and 4
+ #tup =1,2,3,4
+# print(tup)
+
+# Q2. Print the length of the tuple created in Q1.
+# tup =1,2,3,4
+# print(len(tup))
